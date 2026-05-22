5f14bf670d91d934bfb9f05acf6f3b7d5993c3ca
wirisoft pc <wirideveloper@gmail.com>
2026-05-21 18:50:16 -0600

    first commit

---
+++ financiero/src/main/java/com/repaso/financiero/FinancieroApplication.java
@@ -0,0 +1,13 @@
+package com.repaso.financiero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancieroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancieroApplication.class, args);
+	}
+
+}
---
+++ financiero/src/test/java/com/repaso/financiero/FinancieroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repaso.financiero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancieroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
